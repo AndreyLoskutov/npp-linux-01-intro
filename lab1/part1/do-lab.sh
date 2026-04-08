13892a55512ea46306a7235a341813b35a45fed1
a-loskutov-2 <andreyvloskutov@gmail.com>
2026-04-08 14:30:24 -0400

    lab sol

--- lab1/part1/do-lab.sh
+++ lab1/part1/do-lab.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/bash
 
-# INCLUDE ALL COMMANDS NEEDED TO PERFORM THE LAB
-# This file will get called from capture_submission.sh
+# Create a bridge on the switch container
+docker exec clab-lab1-part1-switch ip link add name mybridge type bridge
 
+# Bring the bridge up
+docker exec clab-lab1-part1-switch ip link set mybridge up
+
+# Add all switch ports as bridge members
+docker exec clab-lab1-part1-switch ip link set eth1 master mybridge
+docker exec clab-lab1-part1-switch ip link set eth2 master mybridge
+docker exec clab-lab1-part1-switch ip link set eth3 master mybridge
+docker exec clab-lab1-part1-switch ip link set eth4 master mybridge
+
+# Bring all ports up
+docker exec clab-lab1-part1-switch ip link set eth1 up
+docker exec clab-lab1-part1-switch ip link set eth2 up
+docker exec clab-lab1-part1-switch ip link set eth3 up
+docker exec clab-lab1-part1-switch ip link set eth4 up
